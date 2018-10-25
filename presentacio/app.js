7c9596639bc5048292cba6eee544e3094372a92a
Joan Miquel Torres <joanmi@gmail.com>
2018-10-25 10:24:13 +0200

    Serve-index per a public/resources.
    Pujo preferencies scripts (descuid).

--- presentacio/app.js
+++ presentacio/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var serveIndex = require('serve-index')
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/resources", serveIndex(path.join(__dirname, 'public/resources')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ presentacio/public/resources/build_prefs.sh
@@ -0,0 +1,9 @@
+# npm build / npm unbuild preferences
+
+scss_src="./views/stylesheets/style.scss";
+css_path="./public/stylesheets/style.css";
+css_controllers_file="css_controllers.js";
+
+js_src="views/javascripts/";
+js_path="public/javascripts";
+
